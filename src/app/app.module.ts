32b2afa7d69df8c2482dc11e0e95d19fdc08305b
Damian Arp <damianarp@gmail.com>
2021-01-09 22:19:48 -0300

    Estructura general de la app

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CabeceroComponent } from './cabecero/cabecero.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { EgresoComponent } from './egreso/egreso.component';
+import { FormularioComponent } from './formulario/formulario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabeceroComponent,
+    IngresoComponent,
+    EgresoComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
